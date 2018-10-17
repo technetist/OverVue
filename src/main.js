1133a3f2e97a015e96db77a298c21cbc3562918d
Adrien Maranville <Adrien@Maranville.IO>
2018-10-16 21:50:07 -0600

    added firebase authentication on routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './components/App';
+import Dashboard from './components/Dashboard';
+import Signin from './components/Signin';
+import { firebaseApp } from './firebaseApp';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {path: '/dashboard', component: Dashboard},
+    {path: '/signin', component: Signin}
+  ]
+});
+
+firebaseApp.auth().onAuthStateChanged(user => {
+  if(user) {
+    router.push('/dashboard')
+  } else {
+    router.replace('/signin');
+  }
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   render: h => h(App),
 });
