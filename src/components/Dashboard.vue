09f14b684dae05b83b94833cc1e5254919fc9820
Adrien Maranville <Adrien@Maranville.IO>
2018-10-18 22:07:14 -0600

    added ability to view events

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -7,24 +7,41 @@
             Sign Out
         </button>
         <hr>
-        <AddEvent />
+        <AddEvent/>
         <hr>
+        <div class="col-12">
+            <EventItem
+            v-for="(event_item, index) in this.$store.state.events"
+            :event="event_item"
+            :key="index"/>
+        </div>
     </div>
 </template>
 
 <script>
-    import {firebaseApp} from "../firebaseApp";
-    import AddEvent from './AddEvent';
+  import {firebaseApp, eventsRef} from "../firebaseApp";
+  import AddEvent from './AddEvent';
+  import EventItem from './EventItem';
 
-    export default {
-      methods: {
-        signOut() {
-          this.$store.dispatch('signOut');
-          firebaseApp.auth().signOut();
-        }
-      },
-      components: {
-        AddEvent
+  export default {
+    methods: {
+      signOut() {
+        this.$store.dispatch('signOut');
+        firebaseApp.auth().signOut();
       }
+    },
+    components: {
+      AddEvent,
+      EventItem
+    },
+    mounted() {
+      eventsRef.on('value', snap => {
+        let events = [];
+        snap.forEach(event => {
+          events.push(event.val())
+        });
+        this.$store.dispatch('setEvents', events)
+      })
     }
+  }
 </script>
---
+++ src/components/EventItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col-md-4">
+        <div class="card event-card">
+            <div class="card-title pt-3">
+                <h4>{{ event.title }}</h4>
+            </div>
+            <div class="card-body">
+                <p>{{ event.description }}</p>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Date: {{ event.date }}</li>
+                    <li class="list-group-item">Location: {{ event.location }}</li>
+                    <li class="list-group-item">Host: {{ event.email }}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      props: ['event']
+    }
+</script>
